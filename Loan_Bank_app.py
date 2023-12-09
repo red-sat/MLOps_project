135f4727cb1a71e8f7c5d40864d47beb024855f9
red-sat <reda.benkirane@centrale-casablanca.ma>
2023-12-09 02:20:52 +0100

    Application done

--- Loan_Bank_app.py
+++ Loan_Bank_app.py
@@ -59,8 +59,8 @@ def prediction_api(
 
 # We acknowledge that the number of features is 20, and it's significantly difficult to write all of them in the URL. 
 # Hence we give this example to run : 
-# http://127.0.0.1:5000/reda?checking_account_status=1&duration_months=12&credit_history=1&purpose=2&credit_amount=5000&savings_account=1&employment_duration=2&installment_rate=3&personal_status_sex=1&other_debtors_guarantors=2&residence_duration=4&property=2&age_years=30&other_installment_plans=1&housing=2&existing_credits_at_bank=1&job=3&people_liable_for_maintenance=2&telephone=1&foreign_worker=2
+# http://127.0.0.1:9090/reda?checking_account_status=1&duration_months=12&credit_history=1&purpose=2&credit_amount=5000&savings_account=1&employment_duration=2&installment_rate=3&personal_status_sex=1&other_debtors_guarantors=2&residence_duration=4&property=2&age_years=30&other_installment_plans=1&housing=2&existing_credits_at_bank=1&job=3&people_liable_for_maintenance=2&telephone=1&foreign_worker=2
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=5000)
+    uvicorn.run(app, host="127.0.0.1", port=9090)
